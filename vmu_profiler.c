38befde916e1d8aea526d85b9bde1b494912770c
Falco Girgis <gyrovorbis@gmail.com>
2024-11-15 22:40:25 -0600

    Main thread logic complete.

--- vmu_profiler.c
+++ vmu_profiler.c
@@ -1,10 +1,19 @@
 #include "vmu_profiler.h"
 
+#include <stdlib.h>
+#include <stdatomic.h>
+
+#include <sys/malloc.h>
+
 #include <kos/thread.h>
 #include <kos/rwsem.h>
 
-#include <stdlib.h>
-#include <stdatomic.h>
+#include <arch/arch.h>
+
+#include <dc/sound/sound.h>
+#include <dc/maple.h>
+#include <dc/pvr.h>
+#include <dc/vmu_fb.h>
 
 #define VMU_PROFILER_THREAD_PRIO_DEFAULT_		PRIO_DEFAULT
 #define VMU_PROFILER_POLL_INT_DEFAULT_			200
@@ -14,28 +23,71 @@
 #define VMU_PROFILER_THD_STACK_SIZE				1024
 #define VMU_PROFILER_THD_LABEL					"VmuProfiler"
 
+#define MB_(b)   ((b) * 1024 * 1024)
+
 static struct vmu_profiler {
 	vmu_profiler_config_t config;
 
 	kthread_t *thread;
-	rw_semaphore_t *rwsem;
+	rw_semaphore_t rwsem;
 	atomic_bool done;
 
-	float fps;
-	float ram;
-	float vram;
-	float sram;
 	size_t verts;
 
 	unsigned fps_frame;
 	float fps_frames[];
 } *profiler_ = NULL;
 
+static size_t ram_used_(void) {
+    size_t total_ram = 0; 
+    
+    // Heap 
+    total_ram += mallinfo().uordblks;
+    // Main Stack
+
+    return total_ram;
+}
+
 static void *vmu_profiler_run_(void *arg) {
-	while(!profiler_->done) {
+    struct vmu_profiler* self = arg;
+    maple_device_t* dev = NULL;
 
+	while(!self->done) {
+        thd_sleep(self->config.polling_interval_ms);
 
-		thd_sleep(profiler_->config.polling_interval_ms);
+        if(!(dev = maple_enum_type(0, MAPLE_FUNC_MEMCARD))
+            continue;
+
+        struct mallinfo ram_stats  = ram_used_();
+        size_t          vram_stats = pvr_mem_available();
+        uint32_t        sram_stats = snd_mem_available();
+
+        float fps = 0.0f;
+        for(unsigned f = 0; f < self->config.fps_avg_frames; ++f)
+            fps += self->fps_frames[f];
+        
+        float sh4_mem = (HW_MEMSIZE - ram_stats)  / (float)HW_MEMSIZE * 100.0f;
+        float pvr_mem = (MB_(8)     - vram_stats) / (float)MB_(8)     * 100.0f;
+        float arm_mem = (MB_(2)     - sram_stats) / (float)MB_(2)     * 100.0f;
+
+        if(rwsem_read_lock(&profiler_->rwsem) < 0) {
+            fprintf(stderr, "vmu_profiler_run(): RWSEM lock failed: [%s]!\n", 
+                    strerror(errno));
+            continue;
+        }
+
+        vmu_printf("FPS : %.2f\n"
+                   "SH4 : %.2f%%\n"
+                   "PVR : %.2f%%\n"
+                   "ARM : %.2f%%\n"
+                   "VTX : %lu",
+                   fps, sh4_mem, pvr_mem, 
+                   arm_mem, self->verts);
+
+        if(rwsem_read_unlock(&self->rwsem) < 0) {
+            fprintf(stderr, "vmu_profiler_run(): RWSEM unlock failed: [%s]!\n", 
+                    strerror(errno));
+        }
 	}
 
 	return NULL;
@@ -99,7 +151,7 @@ bool vmu_profiler_start(const vmu_profiler_config_t *config) {
 		.label 			 = VMU_PROFILER_THD_LABEL,	
 	};
 
-	profiler_->thread = thd_create_ex(&attr, vmu_profiler_run_, NULL);
+	profiler_->thread = thd_create_ex(&attr, vmu_profiler_run_, profiler_);
 	
 	if(!profiler_->thread) {
 		fprintf("\tFailed to spawn profiler thread!\n");
